363cccb34eaed6338c40d8b98d8632f6ba97c11c
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-01 02:29:53 +0530

    added the home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Main from './Components/Main';
 import Login from './Components/Login';
 import {Routes,Route} from 'react-router-dom';
 import Signup from './Components/Register';
+import Home from './Components/Home/Home';
+
 
 
 
@@ -11,8 +13,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/dashboard" element={<Main/>}></Route>
-        
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/register" element={<Signup/>}></Route>
         
